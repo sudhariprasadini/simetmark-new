fa7a3b53e2f89d83fc5194fd8d9abe60b9a43ddc
sudhariprasadini <prasadinisudhari@gmail.com>
2025-09-24 10:24:01 +0530

    some change

--- src/components/data/seo.js
+++ src/components/data/seo.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const SEO = ({ pageTitle }) => {
   useEffect(() => {
-    document.title = pageTitle + " - BuildGo - Constructions React Template";
+    document.title = pageTitle + " - Simetmark (Pvt) Ltd";
   }, []);
 };
 
